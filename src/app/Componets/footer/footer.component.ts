0f3e81025c87621a11d22e721d43551fa97a8ea5
unknown <gerardo.magadan@gmail.com>
2022-05-09 11:56:59 -0500

    Footer

---
+++ src/app/Componets/footer/footer.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { faLocationArrow,faMailBulk,faMobilePhone } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  faLocationArrow = faLocationArrow;
+  faMailBulk = faMailBulk;
+  faMobilePhone=faMobilePhone;
+
+  currentYear:number = new Date().getFullYear();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Componets/navbar/navbar.component';
+import { HomeComponent } from './Componets/home/home.component';
+import { FooterComponent } from './Componets/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
